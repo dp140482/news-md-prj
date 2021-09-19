3b2288fff0ffaf6f9400dbbed0fffcf284bd6661
Dmitry <dp_140482@mail.ru>
2021-09-19 11:15:32 +0300

    Версия 19.09.2021
    Замена вложенных кавычек на "ёлочки"

--- js/articles.js
+++ js/articles.js
@@ -64,10 +64,16 @@ class LegalCodesReplacer {
     }
 }
 
+function replaceAt(str, index, replacer) {
+    return str.slice(0, index) + replacer + str.slice(index + 1, str.length);
+}
+
 function extractDataFrom(TextOfBlock) {
     let data = TextOfBlock;
     data = data.replace(/\.\.\./g, '…');
-    data = data.replace(/"([^"]+)"/g, '«$1»');
+    data = data.replace(/(^)\x22(\s)/g, '$1»$2');
+    data = data.replace(/(^|\s|\()"/g, "$1«");
+    data = data.replace(/"(\;|\!|\?|\:|\.|\,|$|\)|\s)/g, "»$1");
     data = data.replace(/(?<!»,) - /g, ' — ');
     return data;
 }
